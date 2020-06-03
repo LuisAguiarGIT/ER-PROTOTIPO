64d0459e6ae1912c6b15155778a4af606231d784
Joel Camacho <joel@outlook.pt>
2020-06-03 13:38:23 +0100

    Recebe dados através de jquery para add ao array Botão so abre com texto seleccionado Zona do modal editado se o estado do array for vazio

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-
+var comentarios = [];
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-
+  res.render('index', { title: 'Express', comentarios:comentarios });
   numbers = req.body.numbers;
   console.log(numbers);
 });
 
-router.post('/ola', (req,res) => {
 
+router.post('/', (req,res) => {
+  
   console.log(req.body.texto);
-  res.send(req.body.texto); 
+  comentarios.push('"'+req.body.texto+'"');
+  console.log(comentarios)
 
 });
 
