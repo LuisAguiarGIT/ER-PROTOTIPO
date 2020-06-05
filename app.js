a8213d5d31e53c364cddca5175598819848b6b92
João Franco <joao_franco13@hotmail.com>
2020-06-05 11:57:07 +0100

    Routes para as paginas login e backoffice

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var backofficeRouter = require('./routes/backoffice');
 
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/backoffice', backofficeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
